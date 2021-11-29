1dfec7201e2718b3186b23d9a10d9beefde5a4e0
NaserHamdan <Alnassre.tnh@hotmail.com>
2021-11-29 16:13:08 +0300

    Use of @auth instead of dirty working it with @guest @else, adding logging out functionality to the mobile version of the button

--- resources/views/Includes/navbar.blade.php
+++ resources/views/Includes/navbar.blade.php
@@ -2,52 +2,52 @@
 
     <div class="max-w-screen-xl mx-auto px-2 sm:px-6 lg:px-8">
         <div class="relative flex items-center justify-between h-16">
-            <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-                <!-- Mobile menu button-->
-                <button type="button"
-                    class="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white mobile-menu-button"
-                    aria-controls="mobile-menu" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <!--
-              Icon when menu is closed.
+            @auth
+                <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                    <!-- Mobile menu button-->
+                    <button type="button"
+                        class="inline-flex items-center justify-center p-2 rounded-md text-white hover:text-white hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white mobile-menu-button"
+                        aria-controls="mobile-menu" aria-expanded="false">
+                        <span class="sr-only">Open main menu</span>
+                        <!--
+                  Icon when menu is closed.
 
-              Heroicon name: outline/menu
+                  Heroicon name: outline/menu
 
-              Menu open: "hidden", Menu closed: "block"
-            -->
-                    <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" aria-hidden="true" id="open">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                    <!--
-              Icon when menu is open.
+                  Menu open: "hidden", Menu closed: "block"
+                -->
+                        <svg class="block h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" aria-hidden="true" id="open">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M4 6h16M4 12h16M4 18h16" />
+                        </svg>
+                        <!--
+                  Icon when menu is open.
 
-              Heroicon name: outline/x
+                  Heroicon name: outline/x
 
-              Menu open: "block", Menu closed: "hidden"
-            -->
-                    <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                        stroke="currentColor" aria-hidden="true" id="close">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M6 18L18 6M6 6l12 12" />
-                    </svg>
-                </button>
-            </div>
+                  Menu open: "block", Menu closed: "hidden"
+                -->
+                        <svg class="hidden h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                            stroke="currentColor" aria-hidden="true" id="close">
+                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                d="M6 18L18 6M6 6l12 12" />
+                        </svg>
+                    </button>
+                </div>
+            @endauth
             <div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
 
                 <div class="hidden sm:block sm:ml-6">
                     <div class="flex space-x-4">
                         <!-- Selected: "bg-red-900 text-white px-3 py-2 rounded-md text-sm font-medium" -->
-                        @guest
-
-                        @else
+                        @auth
                             <a href="#"
                                 class="text-white hover:bg-red-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
                                 href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
-                                              document.getElementById('logout-form').submit();">{{ __('Hello,
-                                                                          Nasser') }}</a>
+                                                  document.getElementById('logout-form').submit();">{{ __('Hello,
+                                                                                                                            Nasser') }}</a>
                             <a href="#"
                                 class="text-white hover:bg-red-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Schedule</a>
 
@@ -62,7 +62,7 @@
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
                                 @csrf
                             </form>
-                        @endguest
+                        @endauth
                     </div>
                 </div>
             </div>
@@ -86,25 +86,31 @@
     </div>
 
     <!-- Mobile menu, show/hide based on menu state. -->
-    <div class="hidden sm:hidden" id="mobile-menu">
-        <div class="px-2 pt-2 pb-3 space-y-1">
-            <!-- selected: "bg-red-900 text-white block px-3 py-2 rounded-md text-base font-medium" -->
-            <a href="#"
-                class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Schedule</a>
+    @auth
+        <div class="hidden sm:hidden" id="mobile-menu">
+            <div class="px-2 pt-2 pb-3 space-y-1">
+                <!-- selected: "bg-red-900 text-white block px-3 py-2 rounded-md text-base font-medium" -->
+                <a href="#"
+                    class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Schedule</a>
 
-            <a href="#"
-                class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Courses</a>
+                <a href="#"
+                    class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Courses</a>
 
-            <a href="#"
-                class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Invigilators</a>
+                <a href="#"
+                    class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Invigilators</a>
 
-            <a href="#"
-                class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Labs</a>
-            <a href="#"
-                class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Log
-                out</a>
+                <a href="#"
+                    class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Labs</a>
+                <a href="#"
+                    class="text-white hover:bg-red-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium"
+                    href="{{ route('logout') }}" onclick="event.preventDefault();
+                                  document.getElementById('logout-form').submit();">{{ __('Log out') }}</a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                    @csrf
+                </form>
+            </div>
         </div>
-    </div>
+    @endauth
     <script>
         // Grab HTML Elements
         const btn = document.querySelector("button.mobile-menu-button");
